817c925a199956aef11cb25cadf9f909423e4a57
schmidtronaldo <schmidt_dev_2024@outlook.com>
2025-01-24 23:59:36 -0300

    ajustes finais css

--- main.js
+++ main.js
@@ -1,23 +1,38 @@
 const display = document.querySelector(".display");
 const teclado = document.querySelector(".teclado");
+display.value = "";
 
 
 const teclas = [
-    '<','(',')','C',
-    '7','8','9','/',
-    '4','5','6','*',
-    '1','2','3','-',
-    '0','.','%','+'
+    '<', '(', ')', 'C',
+    '7', '8', '9', '/',
+    '4', '5', '6', '*',
+    '1', '2', '3', '-',
+    '0', '.', '%', '+'
 ];
 
-teclas.forEach((item)=>{
-    
-    let tecla = document.createElement('button');
-    teclado.appendChild(tecla);
-    tecla.textContent = item;
-    tecla.value = item;
-    tecla.classList.add('tecla');
-    
+teclas.forEach((item) => {
+
+    let btn = document.createElement('button');
+    teclado.appendChild(btn);
+    btn.textContent = item;
+    btn.value = item;
+    btn.classList.add('btn');
+
+});
+
+
+const botoes = document.querySelectorAll('.btn');
+botoes.forEach((btn) => {
+    btn.addEventListener('click', () => {
+       
+            display.value += btn.textContent;
+        
+       
+        
+    })
 })
-    
+
+
+
 
